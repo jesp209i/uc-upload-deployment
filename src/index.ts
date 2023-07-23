5c0115c966c414bacda6ac9bfaa9201163ebe7f1
Jesper Madsen <jam@umbraco.dk>
2023-07-23 15:11:23 +0200

    back to ts again

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,36 @@
 import { getInput, setFailed, info, warning } from '@actions/core';
-import { exec } from '@actions/exec';
+import { exists } from '@actions/io/lib/io-util';
+import { uploadDeployment } from './ucUploadDeploymentSrcApi';
+
 
 async function run() {
-    try {
-        const projectAlias = getInput('project-alias');
-        const apiKey = getInput('api-key');
-        const deploymentId = getInput('deployment-id');
-        const filePath = getInput('file-path');
+    const projectAlias = getInput('project-alias');
+    const apiKey = getInput('api-key');
+    const deploymentId = getInput('deployment-id');
+    const filePath = getInput('file-path');
 
-        const url = `https://api-internal.umbraco.io/projects/${projectAlias}/deployments/${deploymentId}`
-
-        const srcDir = __dirname;
-
-        await exec(`sh ${srcDir}/upload_package.sh ${url} ${apiKey} ${filePath}`)
-
-        info("Source Package uploaded successfully");
-
-    } catch (error) {    
-        warning(`Error: ${error}`);
-
-        setFailed("Got an error while trying to upload source deployment package")
+    const fileExists = await  exists(filePath);
+    if (!fileExists)
+    {
+        warning("File was not found on location");
+        setFailed("File was not found on location");
+        return;
     }
+
+    const url = `https://api-internal.umbraco.io/projects/${projectAlias}/deployments/${deploymentId}/package`;
+
+    uploadDeployment(url, apiKey, filePath)
+        .then(
+        response => {
+            const messages = response.updateMessage.split('\n');
+            const latest = messages.pop();
+            info(`${latest}`);
+            info("Source Package uploaded successfully");
+        }
+        , errorResponse =>{
+            warning(errorResponse);
+            setFailed("Upload failed");
+        });        
 }
 
 run();
---
+++ src/ucUploadDeploymentSrcApi.ts
@@ -0,0 +1,54 @@
+import { request } from 'https';
+import FormData from 'form-data';
+import { createReadStream } from 'fs';
+
+export interface DeploymentResponse {
+    "deploymentState": string,
+    "updateMessage": string
+}
+
+export async function uploadDeployment(callUrl: string, apiKey: string, filePath: string): Promise<DeploymentResponse>
+{
+    return new Promise((resolve, reject) =>{
+        const readStream = createReadStream(filePath);
+
+        const form = new FormData();
+        form.append('file', readStream);
+
+        const headers = form.getHeaders();
+        headers['Umbraco-Api-Key'] = apiKey;
+
+        const requestOptions = {
+            method : 'POST',
+            headers : headers
+        };
+
+        let uploadResponse: DeploymentResponse;
+
+        const req = request(callUrl, requestOptions, 
+            response => {
+                if (response.statusCode !== 202){
+                    const errorMessage = `statusCode: ${response.statusCode}\nStatusMessage: ${response.statusMessage}\nHeaders: ${JSON.stringify(response.headers)}`;
+                    return reject(new Error(errorMessage));
+                }
+            const chunks: any[] = [];
+        
+            response.on('data', chunk => chunks.push(chunk));
+            response.on('end', () => {
+                const data = Buffer.concat(chunks).toString();
+                const obj = JSON.parse(data);
+                const result: DeploymentResponse = {
+                    deploymentState: obj.deploymentState,
+                    updateMessage: obj.updateMessage
+                }
+                return resolve(result);
+            });
+            response.on('error', ()=>{
+                return reject(new Error(JSON.stringify(response)));
+            });
+        });
+
+        form.pipe(req);
+        req.end();
+    });
+}
